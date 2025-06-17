3837b07bf33321064539295da99559b4e25ec5fb
Varsha-Garikapati <varshasahithigarikapati@gmail.com>
2025-06-17 09:19:44 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+# Page Configuration
+st.set_page_config(page_title="Kullhad Economy Festival", layout="wide")
+
+# Hero Section
+st.markdown("# 🌾 KULLHAD ECONOMY FESTIVAL")
+st.markdown("### Reclaiming Roots, Reinventing Futures")
